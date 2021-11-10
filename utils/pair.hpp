50aa5a61179ea37f02f4e7d1fa07e74d7df5dafd
Tomas Sierra paradinas <tsierra-@student.42madrid.com>
2021-11-10 15:09:38 +0100

    Work in pair implementation

--- utils/pair.hpp
+++ utils/pair.hpp
@@ -4,7 +4,26 @@
 namespace	ft
 {
 	template< class T1, class T2 >
-	struct	pair;
+	struct	pair
+	{
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		T1	first;
+		T2	second;
+
+		pair( void ) : first(), second()
+		{};
+
+		pair( const T1& x, const T2& y ) : first( x ), second( y )
+		{};
+
+		template< class U1, class U2 >
+		pair( const pair<U1, U2>& p) : first( p.first ), second( p.second )
+		{};
+
+//		pair( const pair& p );
+	};
 };
 
 #endif
