de392c2d29b43637edebd4094f529a8ebd1f3e6c
Tomas Sierra paradinas <tsierra-@student.42madrid.com>
2021-11-17 20:25:22 +0100

    Add some reverse_iterator operators, still more

--- iterators/ft_reverse_iterator.hpp
+++ iterators/ft_reverse_iterator.hpp
@@ -2,26 +2,107 @@
 # define FT_REVERSE_ITERATOR_HPP
 
 namespace	ft
-{	
+{
 	template< typename Iterator >
-	class	reverse_iterator
-	: public std::iterator< ft::iterator_traits<Iterator>::iterator_category,
+		class	reverse_iterator
+		: public std::iterator< ft::iterator_traits<Iterator>::iterator_category,
 							ft::iterator_traits<Iterator>::value_type,
 							ft::iterator_traits<Iterator>::difference_category,
 							ft::iterator_traits<Iterator>::pointer,
 							ft::iterator_traits<Iterator>::reference >
-	{
-	public:
-		typedef typename Iterator											iterator_type;
-		typedef typename ft::iterator_traits<Iterator>::iterator_category	iterator_category;
-		typedef typename ft::iterator_traits<Iterator>::value_type			value_type;
-		typedef typename ft::iterator_traits<Iterator>::difference_category	difference_category;
-		typedef typename ft::iterator_traits<Iterator>::pointer				pointer;
-		typedef typename ft::iterator_traits<Iterator>::reference			reference;	
+		{
+		protected:
+			Iterator	current;
 
-		/*	default constructor	*/
-		reverse_iterator( void ) {};
-	};
+		public:
+			typedef typename Iterator											iterator_type;
+			typedef typename ft::iterator_traits<Iterator>::iterator_category	iterator_category;
+			typedef typename ft::iterator_traits<Iterator>::value_type			value_type;
+			typedef typename ft::iterator_traits<Iterator>::difference_category	difference_category;
+			typedef typename ft::iterator_traits<Iterator>::pointer				pointer;
+			typedef typename ft::iterator_traits<Iterator>::reference			reference;
+
+			/*	default constructor	*/
+			reverse_iterator( void ) : current()
+			{}
+
+			explicit reverse_iterator( iterator_type x ) : current( x )
+			{}
+
+			template< typename Iter >
+				reverse_iterator( const reverse_iterator<Iter>& other ) : current( other.base() )
+				{}
+
+			template< typename Iter >
+				reverse_iterator&	operator=( const reverse_iterator<Iter>& other )
+				{
+					if ( this = &other )
+						return *this;
+					this->current = other.base();
+					return *this;
+				}
+
+			iterator_type	base( void ) const
+			{
+				return this->current;
+			}
+
+			/*	dereference operator */
+			reference	operator*( void ) const
+			{
+				Iterator	tmp = this-	>current;
+				return *--tmp;
+			}
+
+			pointer	operator->( void ) const
+			{
+				return &(operator*());
+			}
+
+			reverse_iterator&	operator++( void )
+			{
+				--this->current;
+				return *this;
+			}
+
+			reverse_iterator	operator++( int )
+			{
+				reverse_iterator	tmp = *this;
+
+				--this->current;
+				return tmp;
+			}
+
+			reverse_iterator&	operator--( void )
+			{
+				++this->current;
+				return *this;
+			}
+
+			reverse_iterator	operator--( int )
+			{
+				reverse_iterator	tmp = *this;
+
+				++this->current;
+				return tmp;
+			}
+
+			reverse_iterator	operator+( difference_type n ) const
+			{
+				return reverse_iterator( this->base() - n );
+			}
+
+			reverse_iterator	operator-( difference_type n ) const
+			{
+				return reverse_iterator( this->base() + n );
+			}
+
+			reverse_iterator&	operator+=( difference_type n)
+			{
+				this->current -= n;
+				return *this;
+			}
+		};
 };
 
 #endif
